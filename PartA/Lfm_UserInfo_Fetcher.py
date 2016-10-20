832e9cd8d34e7a88989a11bf52db63ffa428c826
Jan Peer <jan.oster94@gmail.com>
2016-10-20 15:09:38 +0200

    updated

--- PartA/Lfm_UserInfo_Fetcher.py
+++ PartA/Lfm_UserInfo_Fetcher.py
@@ -2,15 +2,23 @@ import urllib
 import csv
 import json
 
-USER_FILE = './lastfm_user_5.csv'
+USER_FILE = './lastfm_users_5.csv'
 OUTPUT_DIR = './user_info/'
 
 # generated on last.fm/api
-LASTFM_API_KEY = 'fc2148c763e24a6b0d93f37ae1c7a00c'
+LASTFM_API_KEY = '9bd61f2b7c2931520f8409006d5014a3'
 LASTFM_API_URL = 'http://ws.audioscrobler.com/2.0/'
 
-# def -> function
+# read_user_file def -> function
 def read_user_file(uf):
     users = []
 
-    with open(uf, 'r')
+    with open(uf, 'r') as f:
+        reader = csv.reader(f, delimiter='\t')
+
+        for r in reader:
+            print r[0]
+# /read_user_file
+
+if __name__ == '__main__':
+    read_user_file(USER_FILE)
