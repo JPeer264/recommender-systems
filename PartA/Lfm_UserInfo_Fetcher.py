6b818e884561ff18d4f3375977a8807f13541c63
Jan Peer <jan.oster94@gmail.com>
2016-10-20 15:36:43 +0200

    updated

--- PartA/Lfm_UserInfo_Fetcher.py
+++ PartA/Lfm_UserInfo_Fetcher.py
@@ -1,13 +1,14 @@
 import urllib
 import csv
 import json
+import os
 
-USER_FILE = './lastfm_users_5.csv'
+USER_FILE = './lastfm_users_20.csv'
 OUTPUT_DIR = './user_info/'
 
 # generated on last.fm/api
 LASTFM_API_KEY = '9bd61f2b7c2931520f8409006d5014a3'
-LASTFM_API_URL = 'http://ws.audioscrobler.com/2.0/'
+LASTFM_API_URL = 'http://ws.audioscrobbler.com/2.0/'
 
 # read_user_file def -> function
 def read_user_file(uf):
@@ -17,8 +18,41 @@ def read_user_file(uf):
         reader = csv.reader(f, delimiter='\t')
 
         for r in reader:
-            print r[0]
+            users.append(r[0])
+
+        return users
 # /read_user_file
 
+# lfm_api_call
+def lfm_api_call(user):
+    url  = LASTFM_API_URL + '?method=user.getinfo&user=' + urllib.quote(user) + '&format=json&api_key=' + LASTFM_API_KEY
+    cont = urllib.urlopen(url).read()
+
+    return cont
+# /lfm_api_call
+
+# Main
 if __name__ == '__main__':
-    read_user_file(USER_FILE)
+    users = read_user_file(USER_FILE)
+    print users
+
+    # mkdir in py
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    # Loop over all users
+    for u in users:
+        content   = lfm_api_call(u)
+        ofile     = OUTPUT_DIR + '/' + u + '.json'
+
+        f = open(ofile, 'w')
+        f.write(content)
+        f.close()
+
+        json_user = json.loads(content)
+
+        print json_user['user']['name']
+        print json_user['user']['age']
+        print json_user['user']['gender']
+        print json_user['user']['playlists']
+# /Main
