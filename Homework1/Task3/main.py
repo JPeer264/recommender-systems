ab410ca3efc597f07a93d8788677e4d5d86df738
JPeer264 <jan.oster94@gmail.com>
2016-10-28 14:17:08 +0200

    🔥

--- Homework1/Task3/main.py
+++ Homework1/Task3/main.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn import cross_validation  # machine learning & evaluation module
 import random
 import traceback
+import helper # helper.py
 
 # Parameters
 UAM_FILE = "./data/C1ku_UAM.txt"  # user-artist-matrix (UAM)
@@ -11,6 +12,9 @@ ARTISTS_FILE = "./data/C1ku_idx_artists.txt"  # artist names for UAM
 USERS_FILE = "./data/C1ku_idx_users.txt"  # user names for UAM
 
 NF = 10  # number of folds to perform in cross-validation
+K = 4
+VERBOSE = True
+VERBOSE_DEPTH = 2
 
 
 # Function to read metadata (users or artists)
@@ -25,11 +29,112 @@ def read_from_file(filename):
     f.close()
     return data
 
+def recommend_random_artists_RB(UAM, u_idx, train_aidx):
+    """
+    randomly generates a list of artists which the target_user never heard.
+    It will compare the artists by a random generated user
+
+    :param target_user: the username of the targetuser
+
+    :return: an array with new artists
+    """
+    all_aidx = range(0, UAM.shape[1])
+    u_aidx = np.nonzero(UAM[u_idx, :])[0]
+    print u_aidx
+    random_u_idx = random.sample(np.setdiff1d(all_aidx, u_aidx[train_aidx]), 1)
+
+    print random_u_idx
+
+    # cannot generate the own user
+    if random_u_idx == u_idx:
+        recommend_random_artists_RB(UAM, u_idx)
+
+    u_aidx = np.nonzero(UAM[u_idx,:])[0]
+    random_u_aidx = np.nonzero(UAM[random_u_idx,:])[0]
+
+    # this will return new artists the target_user never heard about
+    return np.setdiff1d(random_u_aidx, u_aidx)
+# /recommend_random_artists_RB
+
+def get_user_neighbors(UAM, user_id):
+    # get playcount vector for current user u
+    pc_vec = UAM[u,:]
+
+    # Compute similarities as inner product between pc_vec of user and all users via UAM (assuming that UAM is already normalized)
+    sim_users = np.inner(pc_vec, UAM)     # similarities between u and other users
+    # print sim_users
+
+    # Sort similarities to all others
+    sort_idx = np.argsort(sim_users)        # sort in ascending order
+    # print sort_idx
+
+    # Select the closest neighbor to seed user u (which is the last but one; last one is user u herself!)
+    # neighbor_idx = sort_idx[k:-1][0] k definiert anzahl der nachbarn
+    neighbor_array = sort_idx[-(K + 1):-1]
+
+    return {
+        'neighbor_array': neighbor_array,
+        'sim_users': sim_users
+    }
+# /get_user_neighbors
+
+def recommend_CF(UAM, user_id, user):
+    """
+    Function that implements a CF recommender. It takes as input the UAM, metadata (artists and users),
+    the index of the seed user (to make predictions for) and the indices of the seed user's training artists.
+    It returns a list of recommended artist indices
+
+    :param UAM:               user-artist-matrix
+    :param seed_uidx:         user index of seed user
+
+    :return: a list of recommended artist indices
+    """
+    user_name = user[user_id]
+
+    users                                  = helper.read_csv(USERS_FILE)
+    artists                                = helper.read_csv(ARTISTS_FILE)
+    artists_obj                            = {}
+    artists_array                          = []
+    artists_obj[user_name]                 = {}
+    artists_obj[user_name]['id']           = user_id
+    artists_obj[user_name]['neighbors_CF'] = {}
+    neighbor_array                         = get_user_neighbors(UAM, user_id)['neighbor_array']
+    sim_users                              = get_user_neighbors(UAM, user_id)['sim_users']
+    artist_idx_u                           = np.nonzero(UAM[u,:]) # indices of artists user u listened to
+    total_artist_rating                    = {}
+
+    for neighbor_index, neighbor in enumerate(neighbor_array, start = 1):
+        a_neighbor = neighbor_array[-(neighbor_index)]
+
+        if VERBOSE and VERBOSE_DEPTH == 2:
+            print '    The ' + helper.number_to_text(neighbor_index) + ' closest user to ' + user_name + ' is ' + users[a_neighbor]
+
+        artist_idx_n  = np.nonzero(UAM[a_neighbor,:]) # indices of artists user u's neighbor listened to
+        artists_array += artist_idx_n[0].tolist()
+
+    artists_unique = helper.get_unique_items(artists_array)
+
+    for artist in artists_unique:
+        artist_count_of_neighbors = 0
+
+        for neighbor_index, neighbor in enumerate(neighbor_array, start = 1):
+            playcount_of_user = UAM[neighbor, artist]
+            rating = playcount_of_user * sim_users[neighbor]
+
+            if artist in total_artist_rating:
+                total_artist_rating[artist] += rating
+            else:
+                total_artist_rating[artist] = rating
+
+    # Return list of 10 recommended artist indices
+    return sorted(total_artist_rating, key=total_artist_rating.__getitem__, reverse=True)[:10]
+# /recommend_CF
+
 
 # Function that implements a CF recommender. It takes as input the UAM, metadata (artists and users),
 # the index of the seed user (to make predictions for) and the indices of the seed user's training artists.
 # It returns a list of recommended artist indices
-def recommend_CF(UAM, seed_uidx, seed_aidx_train):
+def recommend_CF2(UAM, seed_uidx, seed_aidx_train):
     # UAM               user-artist-matrix
     # seed_uidx         user index of seed user
     # seed_aidx_train   indices in UAM of training artists for seed user
@@ -116,7 +221,8 @@ if __name__ == '__main__':
 
     # For all users in our data (UAM)
     no_users = UAM.shape[0]
-    AMOUNT_OF_USER = [1,5,10,20,50,100]
+    # AMOUNT_OF_USER = [1, 5, 10, 20, 50, 100]
+    AMOUNT_OF_USER = [1]
 
     for idx in AMOUNT_OF_USER:
         for u in range(0, idx):
@@ -134,13 +240,15 @@ if __name__ == '__main__':
                         # len(train_aidx)) + ", Test items: " + str(len(test_aidx)),  # the comma at the end avoids line break
                     # Call recommend function
                     copy_UAM = UAM.copy()  # we need to create a copy of the UAM, otherwise modifications within recommend function will effect the variable
-                    rec_aidx = recommend_CF(copy_UAM, u, u_aidx[train_aidx])
-                    print rec_aidx
+
+                    rec_aidx = recommend_CF2(copy_UAM, u, u_aidx[train_aidx])
+                    #rec_aidx = recommend_CF(copy_UAM, u, users[u])
+                    #rec_aidx = recommend_random_artists_RB(copy_UAM, u, train_aidx)
                     #            print "Recommended items: ", len(rec_aidx)
 
                     # For random recommendation, exclude items that the user already knows, i.e. the ones in the training set
-                    #            all_aidx = range(0, UAM.shape[1])
-                    #            rec_aidx = recommend_RB(np.setdiff1d(all_aidx, u_aidx[train_aidx]), len(test_aidx))       # select the number of recommended items as the number of items in the test set
+                    # all_aidx = range(0, UAM.shape[1])
+                    #rec_aidx = recommend_RB(np.setdiff1d(all_aidx, u_aidx[train_aidx]), len(test_aidx))       # select the number of recommended items as the number of items in the test set
 
                     # print "Recommended items: ", len(rec_aidx)
 
