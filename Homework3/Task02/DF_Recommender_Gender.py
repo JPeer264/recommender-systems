6b655b3a81d103de2b3b729917387373053a57c2
Mario Taferner <mario.taferner@gmail.com>
2016-11-21 22:27:17 +0100

    DF Gender

--- Homework3/Task02/DF_Recommender_Gender.py
+++ Homework3/Task02/DF_Recommender_Gender.py
@@ -23,8 +23,8 @@ UAM_FILE = TESTFILES + "C1ku/C1ku_UAM.txt"  # user-artist-matrix (UAM)
 #-----------------------------
 VERBOSE = True
 NF = 10
-MAX_ARTISTS = 1000
-MAX_USERS = 10
+MAX_ARTISTS = 10000
+MAX_USERS = 1050
 MIN_RECOMMENDED_ARTISTS = 0
 METHOD = "DF_test_gender"
 #-----------------------------
@@ -126,23 +126,27 @@ def generate_gender_lists(users):
     :param users: xxx
     :return: xxx
     """
-    users_gender_clean = clean_list_from_empty_value(users, 1, 'n')
+    users_gender_clean = clean_list_from_empty_value(users, 1, '')
     gender_m = {}
     gender_f = {}
+    gender_n = {}
 
     # Checks if the user is male or female and adds them into the correct array
     for user in users_gender_clean:
         if user[1] == 'm':
             gender_m[user[0]] = 'm'
-        else:
+        elif user[1] == 'f':
             gender_f[user[0]] = 'f'
+        else:
+            gender_n[user[0]] = 'n'
 
     if VERBOSE:
         helper.log_highlight("GENDER STATISTICS")
         print "Male: " + str(len(gender_m))
         print "Female: " + str(len(gender_f))
+        print "Neutral: " + str(len(gender_n))
 
-    gender_lists = [gender_m] + [gender_f]
+    gender_lists = [gender_m] + [gender_f] + [gender_n]
 
     return gender_lists
 
@@ -202,7 +206,7 @@ def recommend_gender_DF(UAM, seed_uidx, seed_aidx_train, K):
     # for k=1:
     # artist_idx_n = np.nonzero(UAM[neighbor_idx, :])     # indices of artists user u's neighbor listened to
     # for k>1:
-    artist_idx_n = np.nonzero(UAM[neighbor_idx, :])[
+    artist_idx_n = np.nonzero(UAM[2, :])[
         1]  # [1] because we are only interested in non-zero elements among the artist axis
 
     # Compute the set difference between seed user's neighbor and seed user,
@@ -231,6 +235,9 @@ def recommend_gender_DF(UAM, seed_uidx, seed_aidx_train, K):
         dictlist.append(temp)
 
     sorted_dict_reco_aidx = sorted(dict_recommended_artists_idx.items(), key=operator.itemgetter(1), reverse=True)
+
+    if len(sorted_dict_reco_aidx) < 1:
+        return False
 
     max_value = sorted_dict_reco_aidx[0][1]
 
@@ -266,7 +273,7 @@ def run(_K, _recommended_artists):
     :return: a dictionary of data
     """
 
-    global MIN_RECOMMENDED_ARTISTS
+    global MIN_RECOMMENDED_ARTISTS, UAM_MALE, UAM_FEMALE
 
     # Initialize variables to hold performance measures
     avg_prec = 0
@@ -281,19 +288,28 @@ def run(_K, _recommended_artists):
     for u in range(0, no_users):
 
         user_gender_list = False
+        gender = False
 
         if u in genderLists[0]:
             user_gender_list = genderLists[0]
+            gender = 'm'
         elif u in genderLists[1]:
             user_gender_list = genderLists[1]
+            gender = 'f'
 
         # Only perform test for seed_user who has attribute
         if user_gender_list:
 
             user_with_attr_counter += 1
 
+
+            if gender == 'm':
+               u_aidx = np.nonzero(UAM_MALE[u, :])[0]
+            else:
+               u_aidx = np.nonzero(UAM_FEMALE[u, :])[0]
+
             # Get seed user's artists listened to
-            u_aidx = np.nonzero(UAM[u, :])[0]
+            #u_aidx = np.nonzero(UAM[u, :])[0]
 
             recommended_artists[str(u)] = {}
 
@@ -309,10 +325,19 @@ def run(_K, _recommended_artists):
                     print "User: " + str(u) + ", Fold: " + str(fold) + ", Training items: " + str(
                         len(train_aidx)) + ", Test items: " + str(
                         len(test_aidx)),  # the comma at the end avoids line break
-                # Call recommend function
-                copy_UAM = UAM.copy()  # we need to create a copy of the UAM, otherwise modifications within recommend function will effect the variable
+
+                if gender == 'm':
+                    copy_UAM = UAM_MALE.copy()
+                else:
+                    copy_UAM = UAM_FEMALE.copy()
+
+                #copy_UAM = UAM.copy()  # we need to create a copy of the UAM, otherwise modifications within recommend function will effect the variable
 
                 dict_rec_aidx = recommend_gender_DF(copy_UAM, u, u_aidx[train_aidx], _K)
+
+                if not dict_rec_aidx:
+                    print "jetzt"
+                    continue
 
                 recommended_artists[str(u)][str(fold)] = dict_rec_aidx
 
@@ -361,6 +386,9 @@ def run(_K, _recommended_artists):
     print " Users with attribute: " + str(user_with_attr_counter)
     print "###########################"
 
+    print avg_prec
+    print avg_rec
+
     f1_score = 2 * ((avg_prec * avg_rec) / (avg_prec + avg_rec))
 
     # Output mean average precision and recall
@@ -387,11 +415,13 @@ if __name__ == '__main__':
     artists = read_artists_file(ARTISTS_FILE)
 
     users_gender = read_users_file(USERS_FILE, 5)
-    global genderLists
+    global genderLists, UAM_FILE, UAM_FEMALE
     genderLists = generate_gender_lists(users_gender)
 
-    print genderLists[0]
-    print genderLists[1]
+    if VERBOSE:
+        print genderLists[0]
+        print genderLists[1]
+        print genderLists[2]
 
     if VERBOSE:
         helper.log_highlight('Loading UAM')
@@ -401,28 +431,29 @@ if __name__ == '__main__':
     UAM_MALE = UAM.copy()
     UAM_FEMALE = UAM.copy()
 
-    UAM_MALE[genderLists[0].keys()] = 0.0
-    UAM_FEMALE[genderLists[1].keys()] = 0.0
+    UAM_MALE[genderLists[1].keys(), :] = 0.0
+    UAM_MALE[genderLists[2].keys(), :] = 0.0
+    UAM_FEMALE[genderLists[0].keys(), :] = 0.0
+    UAM_FEMALE[genderLists[2].keys(), :] = 0.0
 
-    print UAM[[5, 17], :6]
-    print UAM_MALE[[5,17], :6]
-    print UAM_FEMALE[[5,17], :6]
+    # print UAM[[5, 17], :6]
+    # print UAM_MALE[[5,17], :6]
+    # print UAM_FEMALE[[5,17], :6]
 
-    # if VERBOSE:
-    #     print 'Successfully loaded UAM'
-    #
-    #
-    # time_start = time.time()
-    #
-    # run_recommender(run, METHOD)  # serial
-    #
-    # time_end = time.time()
-    # elapsed_time = (time_end - time_start)
-    #
-    # print ""
-    # print "Elapsed time: " + str(elapsed_time)
-    #
-    #
+    if VERBOSE:
+        print 'Successfully loaded UAM'
+
+    time_start = time.time()
+
+    run_recommender(run, METHOD)  # serial
+
+    time_end = time.time()
+    elapsed_time = (time_end - time_start)
+
+    print ""
+    print "Elapsed time: " + str(elapsed_time)
+
+
 
 
 
